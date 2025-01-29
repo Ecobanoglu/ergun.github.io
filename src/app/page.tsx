9f9a37a8f10a20770439d973a9a0c347894bd76a
Ergun C <99650140+ergunc-pqsoftware@users.noreply.github.com>
2025-01-29 16:23:36 +0300

    changed metadata file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,6 @@ import type { Metadata } from "next";
 
 import { About, Projects, Abilities, Experience } from "@/components/blocks";
 
-export const metadata: Metadata = {
-  title: "Anasayfa",
-  description: "Anasayfa hakkında bilgi",
-};
-
 export default function Home() {
   return (
     <>
